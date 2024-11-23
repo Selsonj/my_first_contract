06924fb4bca84d3ecdefc49b5f75ac3e33619783
Selson Joao <joaoselson@gmail.com>
2024-11-23 22:33:40 +0100

    Atualizanda a estrutura para um dApp

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,9 +6,9 @@ export async function run(provider: NetworkProvider) {
   const myContract = MainContract.createFromConfig(
     {
       number: 0,
-      address: address("kQDU69xgU6Mj-iNDHYsWWuNx7yRPQC_bNZNCpq5yVc7LiE7D"),
+      address: address("0QDKohuVL8gEwkLVh8pnHIa54B5ejK7hOOCtJ_1jAWrh4J9g"),
       owner_address: address(
-        "kQDU69xgU6Mj-iNDHYsWWuNx7yRPQC_bNZNCpq5yVc7LiE7D"
+        "0QDKohuVL8gEwkLVh8pnHIa54B5ejK7hOOCtJ_1jAWrh4J9g"
       ),
     },
     await compile("MainContract")
